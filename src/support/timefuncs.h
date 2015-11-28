5b2f1e747e4e915749ec9c5e0c4d760898d77438
Curtis Vogt <curtis.vogt@gmail.com>
2015-11-27 23:09:16 -0600

    Removed timeval struct for Windows

--- src/support/timefuncs.h
+++ src/support/timefuncs.h
@@ -7,13 +7,6 @@
 extern "C" {
 #endif
 
-#if defined(_OS_WINDOWS_)
-struct timeval {
-    long long tv_sec;
-    long long tv_usec;
-};
-#endif
-
 DLLEXPORT int timeval_now(struct timeval *now);
 DLLEXPORT double clock_now(void);
 void sleep_ms(int ms);
